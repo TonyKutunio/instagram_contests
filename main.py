79d8552e36ae79b480a82356460722682823e876
TonyKutunio <55698444+TonyKutunio@users.noreply.github.com>
2020-08-01 17:03:01 +0100

    Optimized

--- main.py
+++ main.py
@@ -6,30 +6,18 @@ import random
 import argparse
 
 
-def get_all_comments_data(media_url):
-    media_id = bot.get_media_id_from_link(media_url)
-    media_comments_data = bot.get_media_comments(media_id)
-    return media_comments_data, media_id
-
-
-def get_mentioned_usernames(comment_text):
-    mentioned_usernames = re.findall(r'@(\w+)', comment_text)
-    return mentioned_usernames
-
 
 def check_if_user_exists(username):
     user_id = bot.get_user_id_from_username(username)
-    if not user_id:
-        return False
-    return True
+    return bool(user_id)
 
 
-def get_right_commenters(all_comments_data):
+def get_right_commenters(comments):
     right_commenters = []
-    for comment_data in all_comments_data:
-        comment_text = comment_data['text']
-        commenter_user_id = comment_data['user_id']
-        mentioned_usernames = get_mentioned_usernames(comment_text)
+    for comment in comments:
+        comment_text = comment['text']
+        commenter_user_id = comment['user_id']
+        mentioned_usernames = re.findall(r'@(\w+)', comment_text)
         for mentioned_username in mentioned_usernames:
             if_user_id_true = check_if_user_exists(mentioned_username)
             if if_user_id_true:
@@ -37,11 +25,13 @@ def get_right_commenters(all_comments_data):
     return right_commenters
 
 
-def get_liked_and_followed(media_likers, right_commenters_id, followers_of_media_author):
+def get_liked_and_followed(media_likers,
+                           right_commenters_id,
+                           followers_of_media_author):
     commenters_who_liked = set(media_likers) & set(right_commenters_id)
-    commenters_who_followed = set(commenters_who_liked) & set(followers_of_media_author)
+    commenters_who_followed = set(commenters_who_liked)\
+                              & set(followers_of_media_author)
     return list(commenters_who_followed)
-
 
 def get_winner(commenters_who_followed):
     winner_id = random.choice(commenters_who_followed)
@@ -78,11 +68,12 @@ if __name__ == '__main__':
     media_url = args.url
     media_authors_username = args.username
 
-    all_comments_data, media_id = get_all_comments_data(media_url)
+    media_id = bot.get_media_id_from_link(media_url)
+    comments = bot.get_media_comments(media_id)
     media_likers = bot.get_media_likers(media_id)
     followers_of_media_author = bot.get_user_followers(media_authors_username)
 
-    right_commenters_id = get_right_commenters(all_comments_data)
+    right_commenters_id = get_right_commenters(comments)
     commenters_who_followed = get_liked_and_followed(media_likers,
                                                      right_commenters_id,
                                                      followers_of_media_author)
